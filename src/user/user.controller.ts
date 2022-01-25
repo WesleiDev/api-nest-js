ea3dc138c19e79b82c986943666d6071ce32eac6
Weslei Ferreira da silva <weslei.fs@hotmail.com>
2022-01-25 17:42:21 -0300

    adicionado outros metodos no controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Response } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Response } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/models/user.model';
 import { Repository } from 'typeorm';
@@ -14,5 +14,15 @@ export class UserController {
   store(@Body() body: User): Promise<User> {
     const user = this.userRepo.create(body);
     return this.userRepo.save(user);
+  }
+
+  @Get()
+  async index(): Promise<User[]> {
+    return this.userRepo.find();
+  }
+
+  @Get(':id')
+  show(@Param() id: string): Promise<User> {
+    return this.userRepo.findOneOrFail(id);
   }
 }
